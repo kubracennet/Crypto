18b5e03f7711df232fcc73a8306afe1010b952bd
kubracennet <kubracennetyavasoglu@gmail.com>
2023-04-03 22:11:19 +0300

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Kübra Cennet Yavaşoğlu on 3.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
