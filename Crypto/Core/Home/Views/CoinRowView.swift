262e73c67b8ff1a3d0a5da8de35862f32f03c96e
kubracennet <kubracennetyavasoglu@gmail.com>
2023-04-06 16:25:39 +0300

    Create design coins in a list

---
+++ Crypto/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,22 @@
+//
+//  CoinRowView.swift
+//  Crypto
+//
+//  Created by Kübra Cennet Yavaşoğlu on 6.04.2023.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CoinRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinRowView()
+    }
+}
